0254841684e54ea2ee8de26e765afce799e71921
Dean Nikolov <deannikolov@MBPDeanNikolov.Dlink>
2017-08-27 17:38:19 +0300

    DNproject_HW4_GIT

---
+++ DNproject_HW4_GIT/Gender.swift
@@ -0,0 +1,13 @@
+//
+//  Gender.swift
+//  DNproject_HW4_GIT
+//
+//  Created by Dean Nikolov on 8/25/17.
+//  Copyright © 2017 Dean Nikolov. All rights reserved.
+//
+
+import Foundation
+
+enum  Gender {
+    case male, female
+}
